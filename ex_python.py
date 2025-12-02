5725d52f32522de0bd7764ec469e245a39872f27
daniv-20 <vaithid1@mskcc.org>
2025-12-02 09:54:49 -0500

    actually added stuff to the scripts

--- ex_python.py
+++ ex_python.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import numpy as np
 
-df = ## create lil dataframe
+import matplotlib.pyplot as plt
 
-## export dataframe
+# create lil dataframe
+df = pd.DataFrame({
+    "x": np.arange(10),
+    "y": np.random.randn(10).cumsum()
+})
 
-## make & save plot
+# export dataframe
+df.to_csv("demo_data_py.csv", index=False)
+
+# make & save plot
+plt.figure(figsize=(6,4))
+plt.plot(df["x"], df["y"], marker="o")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Demo Plot")
+plt.tight_layout()
+plt.savefig("demo_plot.png")
+plt.close()
--- ex_r.R
+++ ex_r.R
@@ -1,8 +1,19 @@
 library(tidyverse)
 
-## create dataframe
+# create lil dataframe
+df <- data.frame(
+  x = 0:9,
+  y = cumsum(rnorm(10))
+)
 
-## export dataframe
+# export dataframe
+write.csv(df, "demo_data_R.csv", row.names = FALSE)
 
-
-## create and save plot
+# make & save plot
+png("demo_plot.png", width = 600, height = 400)
+plot(df$x, df$y,
+     type = "o",
+     xlab = "x",
+     ylab = "y",
+     main = "Demo Plot")
+dev.off()
